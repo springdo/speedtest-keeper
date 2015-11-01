a4455a60f128116a95545f8c99c90c2b3a7f6616
donal <donal@vaio.(none)>
2015-11-01 16:04:42 +0000

    skeleton work

---
+++ speed-recorder.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+function run_speed_test() {
+	./speedtest-cli --simple > test_scores.txt
+}
+
+
+function convert_to_csv() {
+
+
+}
+
+
+function save_to_google_drive() {
+
+
+}
+
+
+run_speed_test
+
+
