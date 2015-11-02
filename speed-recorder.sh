0f902cf4ba641f5da646514a1525c36d97544ed9
donal <donal@vaio.(none)>
2015-11-02 01:57:29 +0000

    update counters for reshape of data

--- speed-recorder.sh
+++ speed-recorder.sh
@@ -9,12 +9,12 @@ function run_speed_test() {
 
 function convert_to_csv() {
 	echo "Running convert to csv task"
-	printf `date +%x_%H:%M:%S` >> test_scores.csv
+	printf `date +%x_%H:%M:%S`, >> test_scores.csv
 	readarray -t LINES < test_scores.txt
 	COUNTER=0 
 	for LINE in "${LINES[@]}";
 	do
-		if [ $COUNTER -eq 3 ]; then
+		if [ $COUNTER -eq 2 ]; then
 			printf `echo $LINE | cut -d' ' -f2` >> test_scores.csv 
 		else
 			printf `echo $LINE | cut -d' ' -f2`, >> test_scores.csv
